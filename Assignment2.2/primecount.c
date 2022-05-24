e3c30c75f1dfcbdc354dcf78c4df59d57a0bc2d7
barmoshe <1barmoshe1@gmail.com>
2022-05-24 16:31:36 +0300

    a22

--- Assignment2.2/primecount.c
+++ Assignment2.2/primecount.c
@@ -3,25 +3,55 @@
 #include <stdlib.h>
 #include <math.h>
 #include <unistd.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+#include <pthread.h>
 
-void parseargs(char *argv[], int argc, int *lval, int *uval, int *nval, int *tval);
+void parseargs(char *argv[], int argc, int *lval, int *uval, int *nval, int *pval, int *tval);
 int isprime(int n);
-char *flagarr = NULL;
-int lval = 1;
-int uval = 100;
-int num;
+
+int globalNum;
+
+struct argument
+{
+    char *flagarr;
+    int lval;
+    int uval;
+    pthread_mutex_t mutex;
+    int index;
+};
+
+void *func(void *arguments)
+{
+    struct argument *args = (struct argument *)arguments;
+    while (globalNum <= args->uval)
+    {
+        pthread_mutex_lock(&args->mutex);
+        int mynum = globalNum;
+        globalNum++;
+        pthread_mutex_unlock(&args->mutex);
+        if (isprime(mynum))
+        {
+            args->flagarr[mynum - args->lval] = 1;
+        }
+    }
+}
 
 int main(int argc, char **argv)
 {
+    int lval = 1;
+    int uval = 1000;
     int nval = 10;
+    int pval = 4;
     int tval = 4;
-    int countPrime = 0;
+    char *flagarr = NULL;
+    pthread_t *threads = NULL;
+    int count = 0;
 
-    // Parse arguments
-    parseargs(argv, argc, &lval, &uval, &nval, &tval);
+    parseargs(argv, argc, &lval, &uval, &nval, &pval, &tval);
     if (uval < lval)
     {
-        fprintf(stderr, "Upper bound should not be smaller then lower bound\n");
+        fprintf(stderr, "Upper Erorr\n");
         exit(1);
     }
     if (lval < 2)
@@ -29,67 +59,99 @@ int main(int argc, char **argv)
         lval = 2;
         uval = (uval > 1) ? uval : 1;
     }
+    if (pval <= 0)
+    {
+        fprintf(stderr, "You need to use at least one processor\n");
+        exit(1);
+    }
+    if (nval <= 0)
+    {
+        fprintf(stderr, "You need to use at least one print\n");
+        exit(1);
+    }
+    if (tval <= 0)
+    {
+        fprintf(stderr, "You need to use at least one thread\n");
+        exit(1);
+    }
 
-    // Allocate flags
-    flagarr = (char *)malloc(sizeof(char) * (uval - lval + 1));
+    flagarr = (char *)calloc((uval - lval + 1), sizeof(char));
     if (flagarr == NULL)
         exit(1);
 
-    // Set flagarr
-    for (num = lval; num <= uval; num++)
+    threads = (pthread_t *)malloc(sizeof(pthread_t) * tval);
+    if (threads == NULL)
     {
-        if (isprime(num))
-        {
-            flagarr[num - lval] = 1;
-            countPrime++;
-        }
-        else
-        {
-            flagarr[num - lval] = 0;
-        }
+        fprintf(stderr, "pthread unsuccess\n");
+        exit(1);
     }
 
-    // Print results
-    printf("Found %d primes%c\n", countPrime, countPrime ? ':' : '.');
-    printf("printing %d : \n", nval);
-    int temp = nval;
-    int index = 1;
-    for (int i = lval; i <= uval; i++)
-        if (flagarr[i - lval])
+    pthread_mutex_t mutex;
+    pthread_mutex_init(&mutex, NULL);
+
+    struct argument args;
+    args.flagarr = flagarr;
+    args.lval = lval;
+    args.uval = uval;
+    args.mutex = mutex;
+
+    for (int i = 0; i < tval; i++)
+    {
+        args.index = i;
+        if (pthread_create(threads + i, NULL, &func, (void *)&args) != 0)
+            exit(1);
+    }
+
+    for (int i = 0; i < tval; i++)
+    {
+        if (pthread_join(threads[i], NULL) != 0)
+            perror("failed to join thread!\n");
+    }
+
+    for (int i = 0; i < uval; i++)
+    {
+        if (flagarr[i] == 1)
+            count++;
+    }
+
+    printf("Found %d primes%c\n", count, count ? ':' : '.');
+    for (globalNum = lval; (globalNum <= uval) && (nval != 0); globalNum++)
+        if (flagarr[globalNum - lval])
         {
-            temp--;
-            if (temp >= 0)
-            {
-                printf("%d%c) %d\n", index, index >= 10 ? '\0' : ' ', i);
-                index++;
-            }
+            count--;
+            nval--;
+            printf("%d%c", globalNum, nval ? ',' : '\n');
         }
 
+    pthread_mutex_destroy(&mutex);
     return 0;
 }
 
-// NOTE : use 'man 3 getopt' to learn about getopt(), opterr, optarg and optopt
-void parseargs(char *argv[], int argc, int *lval, int *uval, int *nval, int *tval)
+void parseargs(char *argv[], int argc, int *lval, int *uval, int *nval, int *pval, int *tval)
 {
     int ch;
 
     opterr = 0;
-    while ((ch = getopt(argc, argv, "l:u:n:t:")) != -1)
+    while ((ch = getopt(argc, argv, "l:u:n:p:t:")) != -1)
         switch (ch)
         {
-        case 'l': // Lower bound flag
+        case 'l':
             *lval = atoi(optarg);
             break;
-        case 'u': // Upper bound flag
+        case 'u':
             *uval = atoi(optarg);
             break;
-        case 'n': // Upper bound flag
+        case 'n':
             *nval = atoi(optarg);
+            break;
+        case 'p':
+            *pval = atoi(optarg);
             break;
         case 't':
             *tval = atoi(optarg);
+            break;
         case '?':
-            if (((optopt == 'l') || (optopt == 'u')) || (optopt == 'n'))
+            if ((optopt == 'l') || (optopt == 'u'))
                 fprintf(stderr, "Option -%c requires an argument.\n", optopt);
             else if (isprint(optopt))
                 fprintf(stderr, "Unknown option `-%c'.\n", optopt);
